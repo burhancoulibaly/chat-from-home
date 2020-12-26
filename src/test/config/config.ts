fbebca5af40985414fe5b3125b8459276416ed83
burhancoulibaly <burhancoulibaly@gmail.com>
2020-12-26 03:31:55 -0500

    Setup circleci config file

--- src/test/config/config.ts
+++ src/test/config/config.ts
@@ -18,5 +18,5 @@ export const firebaseConfig =  {
 export const serviceAccount: ServiceAccount =  {
     projectId: `${process.env.PROJECT_ID}`,
     clientEmail: `${process.env.CLIENT_EMAIL}`,
-    privateKey: `${process.env.PRIVATE_KEY}`
+    privateKey: `${process.env.PRIVATE_KEY?.replace(/\n/g,'\n')}`
 }
