4b043baf3e757375af757eef501dfd1e7a47cb5d
burhancoulibaly <burhancoulibaly@gmail.com>
2020-12-26 12:56:55 -0500

    Setup circleci config file

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,6 @@ const { defaults } = require('jest-config');
 module.exports = {
     verbose: true,
     roots: ['<rootDir>'], 
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testPathIgnorePatterns: [
         "./dist",
